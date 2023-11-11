dc6fdaceff52ca78d5f0f54d0e93a89a696e10ab
Toby Shearman <toby.shearman@gmail.com>
2023-11-10 19:32:16 -0500

    initial commit

---
+++ app/years/[year]/page.tsx
@@ -0,0 +1,45 @@
+import { slug } from 'github-slugger'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import siteMetadata from '@/data/siteMetadata'
+import ListLayout from '@/layouts/ListLayoutWithTags'
+import { allBlogs } from 'contentlayer/generated'
+import yearData from 'app/year-data.json'
+import { genPageMetadata } from 'app/seo'
+import { Metadata } from 'next'
+
+export async function generateMetadata({ params }: { params: { year: string } }): Promise<Metadata> {
+    const year = decodeURI(params.year)
+    return genPageMetadata({
+        title: year,
+        description: `${siteMetadata.title} posts from ${year}`,
+        alternates: {
+            canonical: './',
+            types: {
+                'application/rss+xml': `${siteMetadata.siteUrl}/years/${year}/feed.xml`,
+            },
+        },
+    })
+}
+
+export const generateStaticParams = async () => {
+    const yearCounts = yearData as Record<string, number>
+    const yearKeys = Object.keys(yearCounts)
+    const paths = yearKeys.map((year) => ({
+        year: year,
+    }))
+    return paths
+}
+
+export default function TagPage({ params }: { params: { year: string } }) {
+    function toYear(s: string) {
+        return (new Date(Date.parse(s))).getFullYear()
+    }
+
+    // Capitalize first letter and convert space to dash
+    const year = params.year
+    const title = year
+    const filteredPosts = allCoreContent(
+        sortPosts(allBlogs.filter((post) => post.date && `${toYear(post.date)}` == year))
+    )
+    return <ListLayout posts={filteredPosts} title={title} />
+}
---
+++ components/TableOfContents.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import Link from 'next/link'
+
+const TableOfContexts = ({ toc, depth = 2 }) => {
+    return (
+        <div>
+            {toc.filter((entry) => entry.depth <= depth)
+                .map(entry => {
+                    return (
+                        <div key={`#${entry.url}`}>
+                            <Link href={entry.url} data-level={entry.depth} className="mr-3 text-sm font-medium text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                                {entry.value}
+                            </Link>
+                        </div>
+                    )
+                })}
+        </div>
+    )
+}
+
+export default TableOfContexts
